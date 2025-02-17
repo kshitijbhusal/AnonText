363cbfcb3f5e64db447ce1e5bcde035996363e83
Kshitij Bhusal <kshitijbhusal64@gmail.com>
2025-02-17 23:57:09 +0545

    fix db connection

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,8 +1,22 @@
 import mongoose from "mongoose";
 
 export const dbConnect = async () => {
+  const connectionState = mongoose.connection.readyState;
+
+  if (connectionState === 1) {
+    console.log("db already connected.");
+    return;
+  }
+
+  if (connectionState === 2) {
+    console.log("db connecting.");
+    return;
+  }
   try {
-    await mongoose.connect("mongodb://localhost:27017/anon_text");
+    await mongoose.connect(process.env.MONGO_URL!, {
+      dbName: "anon_text",
+      bufferCommands: true,
+    });
     console.log("db connected.");
   } catch (error) {
     console.log("db connection failed.", error);
